1f206c44ddd442caf1750880c99ce81dfded74b3
Vitaliy Bolokan <ki1ativ1992@yandex.ru>
2022-12-20 12:23:24 +0300

    Commit 2 all Diplom 1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,19 +1,42 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+    private String name;
+    private float price;
 
-    Bun bun = new Bun ("MegaBulka", 66.6f);
-
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+    @Parameterized.Parameters(name = "{0}, {1}")
+    public static Object[][] bunData() {
+        return new Object[][]{
+                {"",13.1},
+                {null,111.1},
+                {" ",77.9},
+                {"MegaKrutayaСОченьНевероятноДлиннымНазваниемСостоящихИзБуквLatinicaИКириллицаИ666Цифр",0.1f},
+                {"Булка=из-Spec_simv!ov^:", 333.9f},
+                {"Проверка цены равной ноль", 0},
+                {"Проверка отрицательной цены", -10},
+                {"Проверка минимальной цены", 0.0f},
+                {"Проверка максимальной цены", 9999999999999999999999999999999999999.9f}
+                };
+            }
     @Test
     public void getNameTest () {
-        Assert.assertEquals ("MegaBulka", bun.getName());
+        Bun bun = new Bun (name,price);
+        Assert.assertEquals (name, bun.getName());
     }
 
     @Test
     public void getPriceTest () {
-        Assert.assertEquals (66.6f, bun.getPrice(), 0.0);
+        Bun bun = new Bun (name,price);
+        Assert.assertEquals (price, bun.getPrice(), 0.0);
     }
 
 }
